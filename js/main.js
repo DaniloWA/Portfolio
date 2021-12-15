18bcfae2f9f23aa807ed1c87ddf65d12e055f65d
Danilo Oliveira <DDanilo000@gmail.com>
2021-12-15 17:48:12 +0000

    Fix bug Media and JS slide

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
+let botao = document.getElementsByClassName("btnSlide")
+for(let i = 0; i < botao.length; i++){
+    botao[i].onclick = function() { slide() }
+}
+
+
 function slide(){ //Escolhe foto do slide
     let foto = document.getElementsByTagName('html')[0].getAttribute('slide');
-    console.log("Var foto: " + foto)
     let slide = document.getElementsByTagName('html')[0]
-    let temaTxt = document.getElementById("temaTxt");
+    console.log("Var foto: " + foto)
 
     if(foto == "foto1"){
-        slide.setAttribute("slide", "foto1")
-        return
-    }
-    if(foto == "foto2"){
         slide.setAttribute("slide", "foto2")
         return
     }
-    if(foto == "foto3"){
+    if(foto == "foto2"){
         slide.setAttribute("slide", "foto3")
         return
     }
-    
+    if(foto == "foto3"){
+        slide.setAttribute("slide", "foto1")
+        return
+    }
 }
