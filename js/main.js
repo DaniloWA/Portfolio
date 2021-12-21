3eff33652407f9438f559181857464904a06a8ff
Danilo Oliveira <DDanilo000@gmail.com>
2021-12-21 18:30:45 +0000

    projetos update

--- js/main.js
+++ js/main.js
@@ -4,25 +4,30 @@ import * as contatos from "./contatos.js"
 import * as projetos from "./projetos.js"
 import * as menuDrop from "./menuDropdow.js"
 
+
 let page = document.getElementsByTagName('html')[0].getAttribute('page');
 
 switch(page){
     case "index": 
-    console.log("Case: " + page);
-    window.onload = index.botaoADC(); 
+        console.log("Case: " + page);
+        window.onload = index.botaoADC(); 
+        window.onload = menuDrop.menuSelect();
     break;
 
     case "sobre": 
-    console.log("Case: " + page); 
-   // window.onload = sobre.slide();
+        console.log("Case: " + page); 
+        window.onload = menuDrop.menuSelect();
     break;
 
-    case "contatos": console.log("Case: " + page);
-    window.onload = contatos.sobrePage();
+    case "contatos": 
+        console.log("Case: " + page);
+        window.onload = contatos.sobrePage();
+        window.onload = menuDrop.menuSelect();
     break;    
 
-    case "projetos": console.log("Case: " + page); 
-   // window.onload = projetos.slide();
+    case "projetos": 
+        console.log("Case: " + page); 
+        window.onload = menuDrop.menuSelect();
     break;
 }
 
--- js/menuDropdow.js
+++ js/menuDropdow.js
@@ -1,14 +1,18 @@
 console.log("Confirm MenuDropDow") 
 
-const sb = document.querySelector('#menu')
+export function menuSelect(){
+    const sb = document.querySelector('#menu')
 
-sb.onchange = (event) => {
-    event.preventDefault();
-    switch(sb.value){
-        case "Home": window.location ='index.html'; break;
-        case "Sobre": window.location ='sobre.html'; break;
-        case "Contatos": window.location ='contatos.html'; break;
-        case "Projetos": window.location ='projetos.html'; break;
+    sb.onchange = (event) => {
+        event.preventDefault();
+        switch(sb.value){
+            case "Home": window.location ='index.html'; break;
+            case "Sobre": window.location ='sobre.html'; break;
+            case "Contatos": window.location ='contatos.html'; break;
+            case "Projetos": window.location ='projetos.html'; break;
+        }
     }
 }
+
+
 
